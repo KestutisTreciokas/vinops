da34a7351855a768a8182c294b222c68ae57ed2f
kestutistreciokas <vinops.online@hotmail.com>
2025-09-16 23:38:21 +0300

    fix(vin page): Specs expects {specs}; VinGallery uses data.photos

--- frontend/src/app/[lang]/vin/[vin]/page.tsx
+++ frontend/src/app/[lang]/vin/[vin]/page.tsx
@@ -25,7 +25,7 @@ export default function VinPage({ params }: { params: { lang: 'ru'|'en', vin: st
 
         {/* Правая колонка: характеристики и инфо по лоту */}
         <div className="lg:col-span-5 space-y-6">
-          <Specs lang={lang} data={data} />
+          <Specs specs={data.specs} />
           <LotInfo history={data.history} lang={lang} data={data} />
         </div>
 
