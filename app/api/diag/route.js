bdce741247cf23731d0a8322bdc113151bee4333
kestutistreciokas <vinops.online@hotmail.com>
2025-09-17 07:24:42 +0300

    diag: add /api/diag route

---
+++ app/api/diag/route.js
@@ -0,0 +1,3 @@
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+export async function GET(){ return new Response(JSON.stringify({ diag:true }), { headers: { "content-type":"application/json" } }); }
